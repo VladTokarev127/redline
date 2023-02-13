7f27df098f1bd9b4fe651ce8c367bd9a5dab01c1
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-02-13 14:11:31 +0500

    done header & hero

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
- // JS - код
+	const heroSwiper = new Swiper('.hero__swiper', {
+		slidesPerView: 1
+	});
+
+	$('.header__btn').click(function(e) {
+		e.preventDefault();
+		$('body').toggleClass('is-fixed');
+		$('.header__main').fadeToggle(300);
+	});
 
 });
